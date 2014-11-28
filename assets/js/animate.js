09cb0d6b6d07d661701d9f8805a9f8fc4e9be0dd
shrig94 <shrig94@gmail.com>
2014-11-27 22:00:58 -0400

    animations

---
+++ assets/js/animate.js
@@ -0,0 +1,41 @@
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function setFadeTimeout(remove, add, delay, elem)
+{
+	setTimeout(function(){
+		$(elem).delay(delay).removeClass(remove).addClass(add);
+	}, delay);
+}
+
+function runFadeIn(remove, add)
+{
+	var elems = $("."+remove);
+	var delay = 0;
+	for (var i = 0; i < elems.length; i++)
+	{
+		var elem = elems[i];
+		if (isScrolledIntoView(elem))
+		{
+			setFadeTimeout(remove, add, delay, elem)
+			delay = delay + 150;
+		}
+	}
+}
+
+$(document).scroll(function(e)
+{
+	runFadeIn("fadeinlater", "animated fadeIn");
+});
+
+$(document).on("ready", function(){
+	runFadeIn("fadeinlater", "animated fadeIn");
+});
